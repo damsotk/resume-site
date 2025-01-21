63154efe9590190fe185a82c94ccac220ce811b7
damsot <denispiyack@gmail.com>
2025-01-21 16:56:16 +0100

    exchange is ready

--- src/main-page/main-page-show-logic-work/exchange/exchangeTransactions/exchangeTransactions.js
+++ src/main-page/main-page-show-logic-work/exchange/exchangeTransactions/exchangeTransactions.js
@@ -30,6 +30,19 @@ const ExchangeTransactions = () => {
         return <div>{error}</div>;
     }
 
+    function formatDate(isoDateStr) {
+        const date = new Date(isoDateStr);
+
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+
+        return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+    }
+
     return (
         <div className='exchangeBackground'>
             <div className='exchangeFlex'>
@@ -40,16 +53,21 @@ const ExchangeTransactions = () => {
                             <div className='exchangeTextForTrans'>
                                 YOUR TRANSACTION
                             </div>
-                            {transactions.map(transaction => (
-                                <div key={transaction.id}>
-                                    <p>назва: {transaction.stock_name}</p>
-                                    <p>кількість: {transaction.quantity}</p>
-                                    <p>дата: {transaction.transaction_date}</p>
-                                    <p>тип: {transaction.transaction_type}</p>
-                                    <p>за яку суму куплено: {transaction.purchase_price}</p>
-                                    <p>-------------------</p>
-                                </div>
-                            ))}
+                            <div className='allTransactionCards'>
+                                {transactions.map(transaction => (
+                                    <div className='transactionCard' key={transaction.id}>
+                                        <div className='transactionCardNameDate'>
+                                            <div className='transactionNameStock'>{transaction.stock_name}</div>
+                                            <div className='transactionDate'>{formatDate(transaction.transaction_date)}</div>
+                                        </div>
+                                        <div className='transactionStockQuantity'>{transaction.quantity}</div>
+                                        <div className='transactionCardTypePurchase'>
+                                            <div className='transactionType'>{transaction.transaction_type}</div>
+                                            <div className='transactionPurchase'>purchase price {transaction.purchase_price}</div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
